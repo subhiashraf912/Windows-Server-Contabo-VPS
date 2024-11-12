9277eaf5ec09c781418d30eb8faeede0c263f2c4
Sobhi Ashraf <59281574+subhiashraf912@users.noreply.github.com>
2024-11-12 03:12:06 -0800

    Update windows-install.sh

--- windows-install.sh
+++ windows-install.sh
@@ -64,7 +64,7 @@ cd /root/windisk
 
 mkdir winfile
 
-wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://bit.ly/3UGzNcB
+wget -O win10.iso --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US
 
 mount -o loop win10.iso winfile
 
